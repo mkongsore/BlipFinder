182ab1f11a1c1ec945985a7ab0824c3cfca77b36
Marius Kongsoere <marius.kongsore@gmail.com>
2022-12-17 19:44:53 -0500

    updated paths in free fit script

--- Pipeline/Analysis/free_fit.py
+++ Pipeline/Analysis/free_fit.py
@@ -1,22 +1,18 @@
 # GAIA Free Trajectory Analysis Script
-# Calvin Chen, Marius Kongsore, Ken Van Tilburg
-# 2022
+# I-Kai Chen, Marius Kongsore, Ken Van Tilburg
+# December 2022
 
 # This file as part of a larger GitHub package
-# https://github.com/mkongsore/gaiablip
+# https://github.com/mkongsore/BlipFinder
 
 ############################
 # Load Functions and Files #
 ############################
  
-# Specify the directory where results are to be saved
-results_path = '/scratch/mk7976/fit_results/x1_new/free_fit_results'
-
-# Specify the folder to load the catalog from
-catalog_path = '/scratch/mk7976/epoch_astrometry/lens_new'
-
-# Specify the folder in which the analysis is run
-analysis_path = '/home/mk7976/git/gaiablip/analysis'
+# File paths
+data_path = '../Data' # Specify folder to load data from
+source_info_path = '../SourceInfo' # Specift path to read EDR3 source info from
+results_path = '../Results/FreeScipy' # Specify path to save results to
 
 # Import system packages
 import sys
@@ -26,8 +22,8 @@ import os
 import scipy as sp
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
 
+# Import BlipFinder analysis functions
 import analysis_fcns # Import the analysis functions script
 import constraint_fcns # Import the constraint functions script
 
@@ -36,23 +32,15 @@ analysis = analysis_fcns.blip_search('black_holes')
 
 # Initialize constraint functions
 free_constraints = constraint_fcns.cons_free # Tuple of functions contraining free model fitting
-blip_constraints = constraint_fcns.cons_blip # Tuple of functions for constraining lensed model fitting
-
-# Change system path
-os.chdir('..') # Go to the parents directory
-cwd = os.getcwd() # Retrieve directory of current running processes
-sys.path.insert(0, cwd) # Change the system path to the current running directory
 
 # Import fcns scripts
 import dynamics_fcns # Import the dynamics functions script
 import bh_prior_fcns # Import the black hole prior functions script
 
 # Change system path
-os.chdir(analysis_path) # Go to the analysis directory
-cwd = os.getcwd() # Retrieve directory of current running processes
-sys.path.insert(0, cwd) # Change the system path to the current running directory
+sys.path.append('../')
 
-# Initialize classes
+# Import BlipFinder dynamics and prior functions
 dynamics = dynamics_fcns.Dynamics() # Functions for stellar and lens motion
 priors = bh_prior_fcns.BH_priors() # Functions for black hole priors
 
@@ -62,36 +50,31 @@ print('Functions Initialized')
 job_idx = int(sys.argv[1])
 
 # Load file containing time and scan angle information for all observations
-obs_info = pd.read_csv('../obs_info.csv', sep=",", header=None, skiprows = [0]) # Read observation info csv
-obs_info.columns = ['t_obs','scan_angles'] # Specify what each column in obs_info file are
+obs_info = pd.read_csv('../obs_info.csv', sep=",") # Read observation info csv
 
-# Extrapolate info from obs_info file
-t_obs = obs_info['t_obs'].to_numpy() # Observation times [Julian years]
-scan_angles = obs_info['scan_angles'].to_numpy() #  Scan angles [rad]
+# Obtain general Gaia observation info from obs_info file
+t_obs = obs_info['t_obs [Julian Year]'].to_numpy() # Observation times [Julian years]
+scan_angles = obs_info['Scan Angles [rad]'].to_numpy() #  Scan angles [rad]
 t_ref = 2017.5 # Reference time for calculating displacement [Julian years]
-n_obs = len(t_obs) # Number of observations
 
-file_name = os.listdir(catalog_path)[job_idx] # Pick the file name corresponding to the job to be ananalyzed
-file_number = file_name[18:31] # Pick the data file number from the data file name
-info_file_name = 'gaia_info_'+file_number+'.csv' # Initialize the name of the info file correpsponding to the data file
+file_name = os.listdir(catalog_path)[job_idx] # Pick the file name corresponding to the job to be analyzed
+file_id = str(file_name[11:24]) # Pick the data file number from the data file name
+info_file_name = 'gaia_info_'+file_id+'.csv' # Initialize the name of the info file correpsponding to the data file
 
 
 # Load data file containing the displacement-time coordinates
-data = catalog_path+'/gaia_epoch_lensed_'+file_number+'_new.pkl' # Specify the location of the obs files
+data = data_path+'/gaia_epoch_'+file_id+'.pkl' # Specify the location of the obs files
 data = pd.read_pickle(data) # Load observation data from pickle file
-
-# Load info file, containing supplemetary information about each observation
-misc_info_folder = '/scratch/ic2127/gaia_edr3_info/' # Specify the location of the folder containing the file with parallax and g magnitude data
-misc_info_data = pd.read_csv(misc_info_folder+info_file_name) # Load in file containing parallax and g magnitude information
+source_info = pd.read_csv(source_info_path+info_file_name) # Load in file containing parallax and g magnitude information
 
 # Load random seed list
-seed_info = pd.read_csv('/scratch/mk7976/seed_lists/'+file_number+'_seeds.csv')
+seed_info = pd.read_csv('./'+file_number+'_seeds.csv')
 
 print('Files Loaded')
 
-#######################
-# RUN SOURCE ANALYSIS #
-#######################
+###########################
+# RUN FREE MODEL ANALYSIS #
+###########################
 
 print('Analysis Loop Commenced')
 
@@ -142,12 +125,8 @@ for n in np.arange(0,np.shape(data)[0]): # n is the row number
                                      options = {'maxiter':1000000}, # Set the miximum number of minimizer iterations
                                      )
 
-    # Save best fit values
-    s_free_ra0,s_free_dec0,s_free_pm_ra, s_free_pm_dec,s_free_d = free_fit.x # Extract best fit parameters
-    free_2ll = free_fit.fun # Save the log likelihood of the free fit to the data
-
     # Save the best fit parameters and source id to a dictionary
-    data_out = {'id':[s_id],'y0':[free_fit.x[0]],'y1':[free_fit.x[1]],'y2':[free_fit.x[2]],'y3':[free_fit.x[3]],'y4':[free_fit.x[4]],'2ll':[free_2ll]}
+    data_out = {'id':[s_id],'y0':[free_fit.x[0]],'y1':[free_fit.x[1]],'y2':[free_fit.x[2]],'y3':[free_fit.x[3]],'y4':[free_fit.x[4]],'-2ll':[free_fit.fun]}
 
     # Convert the output dictionary to a pandas dataframe
     dataf = pd.DataFrame(data_out)
